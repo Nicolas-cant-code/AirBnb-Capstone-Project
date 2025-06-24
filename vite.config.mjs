6f27c72ee3f2ef65757cd63aaa9d07740c01cfbf
Nicolas Webb <nicw103@gmail.com>
2025-06-24 18:25:59 +0200

    Added more build stuff in my vite.config

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   publicDir: "public",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   // server: {
   //   proxy: {
