810fd318c11ef543fd969ed77c5b6d2123dda6df
Nicolas Webb <nicw103@gmail.com>
2025-06-25 17:39:35 +0200

    fixed image pathing

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 
 const storageOptions = Multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/assets/Airbnbs" + file.fieldname);
+    cb(null, "/assets/Airbnbs" + file.fieldname);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
